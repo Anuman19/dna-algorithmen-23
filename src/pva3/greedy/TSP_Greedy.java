7cc65375c2135280ed169cd822a9f6303af17d97
nicolas.dreier@students.ffhs.ch <nicolas.dreier@students.ffhs.ch>
2022-09-21 20:36:03 +0200

    first draft for checkInput method

--- src/pva3/greedy/TSP_Greedy.java
+++ src/pva3/greedy/TSP_Greedy.java
@@ -1,9 +1,7 @@
 package pva3.greedy;
 
-public class TSP_Greedy
-{
-    static int[] findPath(int[][] adja, int start)
-    {
+public class TSP_Greedy {
+    static int[] findPath(int[][] adja, int start) {
         checkInput(adja, start);
         // TODO
         return null;
@@ -14,11 +12,27 @@ public class TSP_Greedy
      * + Die Adjazenzmatrix muss quadratisch sein
      * + Die Matrizenelemente müssen >= 0 sein
      * + Der Startwert muss eine zulässige Knotennummer sein
+     *
      * @throws IllegalArgumentException
      */
-    static void checkInput(int[][] adja, int start)
-            throws IllegalArgumentException
-    {
+
+    static void checkInput(int[][] adja, int start) throws IllegalArgumentException {
+        if (adja.length != adja[0].length) {
+            throw new IllegalArgumentException("Keine quadratisch Matrix");
+        } else if (start < 0 || start >= adja.length) {
+            throw new IllegalArgumentException("Start nicht valide");
+            // native method checkIndex testen
+        } else {
+            for (int i = 0; i < adja.length; i++) {
+                for (int j = 0; j < adja.length; j++) {
+                    if (i == j && (adja[i][j] != 0)) {
+                        throw new IllegalArgumentException("Diagonale nicht null");
+                    } else if (i != j && (adja[i][j] <= 0)) {
+                        throw new IllegalArgumentException("Nicht Diagonalenelement ist = 0");
+                    }
+                }
+            }
+        }
         // TODO
     }
 }
