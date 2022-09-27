51589fe42b3871fdb5cb6085670f4f55cf8e82d6
nicolas.dreier@students.ffhs.ch <nicolas.dreier@students.ffhs.ch>
2022-09-27 13:32:57 +0200

    added Comments

--- src/pva3/greedy/TSP_Greedy.java
+++ src/pva3/greedy/TSP_Greedy.java
@@ -4,15 +4,19 @@ public class TSP_Greedy {
     static int[] findPath(int[][] adja, int start) {
         checkInput(adja, start);
 
+        // array that saves the chosen path
         int[] visited = new int[adja.length];
 
+        // saves the current location from which we search the next step
         int current = start;
 
         for (int i = 0; i < adja.length; i++){
-            int min = 2147483647;
+            int min = Integer.MAX_VALUE;
             int minPlace = 0;
 
+            // basic minSearch to find the shortest next step
             for (int j = 0; j < adja.length; j++){
+                // checks for 0 (self to self) and already visited locations
                 if (adja[current][j] != 0 && !arrayContains(visited, j)){
                     if (adja[current][j] < min){
                         min = adja[current][j];
@@ -21,15 +25,14 @@ public class TSP_Greedy {
                 }
             }
 
+            // store location in visited and update current to search for the next location
             visited[i] = minPlace;
             current = minPlace;
         }
-
-        // adja[start] kleinster eintrag finden ausser 0
-        System.out.println(visited);
         return visited;
     }
 
+    // Helper Method to check if an array contains an int. to avoid transforming to Arraylist
     static boolean arrayContains(int[] array, int value){
         for (int i = 0; i < array.length; i++){
             if (array[i] == value){
@@ -48,23 +51,22 @@ public class TSP_Greedy {
      * @throws IllegalArgumentException
      */
 
+    // testInput for cases that are invalid
     static void checkInput(int[][] adja, int start) throws IllegalArgumentException {
         if (adja.length != adja[0].length) {
             throw new IllegalArgumentException("Keine quadratisch Matrix");
         } else if (start < 0 || start >= adja.length) {
-            throw new IllegalArgumentException("Start nicht valide");
-            // native method checkIndex testen
+            throw new IllegalArgumentException("Start value nicht valide");
         } else {
             for (int i = 0; i < adja.length; i++) {
                 for (int j = 0; j < adja.length; j++) {
                     if (i == j && (adja[i][j] != 0)) {
                         throw new IllegalArgumentException("Diagonale nicht null");
                     } else if (i != j && (adja[i][j] <= 0)) {
-                        throw new IllegalArgumentException("Nicht Diagonalenelement ist = 0");
+                        throw new IllegalArgumentException("Nicht Diagonalelement ist = 0");
                     }
                 }
             }
         }
-        // TODO
     }
 }
