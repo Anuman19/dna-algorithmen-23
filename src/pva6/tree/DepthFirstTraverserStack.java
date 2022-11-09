150ef67b315e0db0bf6e8d6b0aaf186b7a34ac8c
naveen.anpalagan@students.ffhs.ch <naveen.anpalagan@students.ffhs.ch>
2022-11-09 20:25:33 +0100

    aufgabe 2 - draft; test passed

--- src/pva6/tree/DepthFirstTraverserStack.java
+++ src/pva6/tree/DepthFirstTraverserStack.java
@@ -1,28 +1,55 @@
 package pva6.tree;
 
+import java.util.Stack;
+
 /**
- * Klasse zum Traversieren eines Baumes mit Tiefensuche.
- * Diese Implementierung verwende keine Rekursion, sondern einen Stack.
+ * Klasse zum Traversieren eines Baumes mit Tiefensuche. Diese Implementierung verwende keine
+ * Rekursion, sondern einen Stack.
  *
  * @param <N>
  */
-public abstract class DepthFirstTraverserStack<N> 
-{
-	/**
-	 * Traversiert einen Baum mit Tiefensuche.
-	 * @param Die Wurzel des zu traversierenden Baumes.
-	 */
-	public void traverse(TreeNode<N> root) 
-	{
-		//TODO
-	}
-	
-	/**
-	 * Operation auf einem Knoten bei der Traversierung; 
-	 * die Operation wird durchgeführt,
-	 * bevor die Nachkommen besucht werden.
-	 * @param value
-	 */
-	abstract protected void visitNode(N value);   
+public abstract class DepthFirstTraverserStack<N> {
+
+  /**
+   * Traversiert einen Baum mit Tiefensuche.
+   *
+   * @paramDie Wurzel des zu traversierenden Baumes.
+   */
+  public void traverse(TreeNode<N> root) {
+    //TODO
+
+    // stack for nodes
+    Stack<TreeNode<N>> nodes = new Stack<>();
+
+    // push root node on stack
+    nodes.push(root);
+
+    // loop until stack is empty
+
+    while (!nodes.empty()) {
+
+      // pop current node from stack and store
+      TreeNode<N> current = nodes.pop();
+
+      // check if current node has been traversed
+      if (current != null) {
+        visitNode(current.value());
+
+        // store all children of current node from right to left
+        for (int i = current.children().size() - 1; i >= 0; i--) {
+          nodes.add(current.children().get(i));
+        }
+      }
+    }
+
+  }
+
+  /**
+   * Operation auf einem Knoten bei der Traversierung; die Operation wird durchgeführt, bevor die
+   * Nachkommen besucht werden.
+   *
+   * @param value
+   */
+  abstract protected void visitNode(N value);
 
 }   
